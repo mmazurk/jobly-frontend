2dd3896fb0f7014e3f22c6a3c0cbdd199d78f09a
markmazurk <markmazurk@gmail.com>
2023-08-20 18:58:50 -0700

    fixed logout

--- src/Logout.js
+++ src/Logout.js
@@ -1,11 +1,24 @@
-function Logout({logout}) {
+import { useNavigate } from "react-router-dom";
 
-    return (
-        <div>
-        <h1 className="mb-3">This is the Logout page.</h1>
-        <button onClick={() => logout()}>Log Out</button>
-        </div>
-    )
+function Logout({ logout }) {
+    const navigate = useNavigate();
+
+    function logoutReturn() {
+        logout();
+        navigate("/");
+    }
+
+  return (
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: "50vh" }}
+    >
+      <div className="text-center">
+        <h2>Are you sure you want to log out?</h2>
+        <button className="btn btn-primary" onClick={logoutReturn}>Log Out</button>
+      </div>
+    </div>
+  );
 }
 
 export default Logout;
